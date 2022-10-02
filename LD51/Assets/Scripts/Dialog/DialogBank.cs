687a9a59e5efc5dbbd63460477764b5f25350b2c
BlueCalx <romain.schoenberger@gmail.com>
2022-10-03 01:48:14 +0200

    New Kelp model + dialogs GreenCalx

--- LD51/Assets/Scripts/Dialog/DialogBank.cs
+++ LD51/Assets/Scripts/Dialog/DialogBank.cs
@@ -4,13 +4,23 @@ using System;
 
 public static class DialogBank
 {
-    public static readonly string[] TEST_DIALOG= 
+    public static readonly string[] CAPTAIN1= 
     {
         "Saturday, April 21th : Log 210432:19.34",
         "We're on a mission to explore Mariana Trench's.",
         "A problem with the Ballast Tanks occured, we're stuck down here.",
         "We found a cavern, it's the only way to keep going forward.",
         "It seems like our light is no use here...",
+    };
+
+    public static readonly string[] BOTANIST1= 
+    {
+        "Wow... I never saw thick Kelps like this before."
+    };
+
+    public static readonly string[] BIOLOGIST1= 
+    {
+        "Careful with those weird starfishes ! Ever heard of Dosidicus gigas ?"
     };
 
 
@@ -20,7 +30,9 @@ public static class DialogBank
     {
         // load dialog in bank
         bank = new List<string[]>{
-             TEST_DIALOG 
+             CAPTAIN1,
+             BOTANIST1,
+             BIOLOGIST1
              };
     }
 
