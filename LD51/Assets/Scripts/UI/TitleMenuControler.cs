472e357491a94d684502ab2fc42c378cc3db9db4
BlueCalx <romain.schoenberger@gmail.com>
2022-10-03 21:52:06 +0200

    Title screen GreenCalx

--- LD51/Assets/Scripts/UI/TitleMenuControler.cs
+++ LD51/Assets/Scripts/UI/TitleMenuControler.cs
@@ -5,16 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class TitleMenuControler : MonoBehaviour
 {
+    private float elapsedTime = 0f;
+    public float lock_duration;
     // Start is called before the first frame update
     void Start()
     {
-        
+        elapsedTime = 0f;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime+=Time.deltaTime;
+        if (elapsedTime>lock_duration)
+        {
+            if (Input.anyKey)
+            {
+                LoadGame();
+            }
+        }
     }
 
     public void LoadGame() {
